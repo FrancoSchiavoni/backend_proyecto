95736bec371952b7cedb30c525fcd35355d097c2
juanchicruzz <juancruzortegacoldorf@gmail.com>
2025-07-15 17:57:50 -0300

    not null client id

--- db/client.py
+++ db/client.py
@@ -2,7 +2,7 @@ from sqlmodel import Session, create_engine
 
 DATABASE_URL = "sqlite:///./database.db"  # Cambiar si usás Postgres/MySQL
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 def get_session():
     with Session(engine) as session:
